96585ec24e4fbd07b0a7a5dc8eab77707159d265
nikila62@ <nikilakumar62@gmail.com>
2025-09-19 14:17:57 +0530

    datatypes

---
+++ datatype.py
@@ -0,0 +1,14 @@
+#numeric 1.int 2.float 3. complex
+a=18
+b=9.56
+print(a,b,type(a),type(b))
+#boolean 
+life=True
+n=10>2
+print(life,n,type(life),type(n))
+#string 
+f="day1 sept 18 "
+print(f,type(f))
+#none
+m=None
+print(m,type(m))
