ae4d6a0fd8757a2f55d369f9de7219caf6d21d4b
nikila62@ <nikilakumar62@gmail.com>
2025-09-23 21:08:21 +0530

    if user gives one input ..it ask again i added if condition and 2 inputs

--- sysargv.py
+++ sysargv.py
@@ -1,5 +1,9 @@
 import sys
+if len(sys.argv)==2:#if i give one input it gives error so I inform this print and exit and ask again
+    print("please enter first name and last name ...")
+    sys.exit()
 name=sys.argv[1]
-email=name.lower().replace(" ",".")+"@company.com"
+last=sys.argv[2]
+email=name.lower().replace(" ",".")+last.lower()+"@company.com"
 print("name:",name)
 print("email:",email)
