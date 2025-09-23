c68e234685ec693817177b81177199b12fcc3e26
nikila62@ <nikilakumar62@gmail.com>
2025-09-23 21:16:58 +0530

    what if user give more than 2 inputs

--- sysargv.py
+++ sysargv.py
@@ -2,7 +2,7 @@ import sys
 if len(sys.argv)==2:#if i give one input it gives error so I inform this print and exit and ask again
     print("please enter first name and last name ...")
     sys.exit()
-name=sys.argv[1]
+name=" ".join(sys.argv[1:])#for get input lot
 last=sys.argv[2]
 email=name.lower().replace(" ",".")+last.lower()+"@company.com"
 print("name:",name)
